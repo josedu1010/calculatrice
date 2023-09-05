c0deba0d5453267af9bdf03793ef8f31e934e2ce
josedu1010 <joserhuayta@gmail.com>
2023-09-05 10:30:43 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calculatrice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            do
+            { 
+            Console.WriteLine("enter number");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+           
+            
+
+
+            Console.WriteLine("a = addition");
+            Console.WriteLine();
+
+            Console.WriteLine("s = soustraction");
+            Console.WriteLine();
+
+            Console.WriteLine("m = multiplication");
+                Console.WriteLine();
+
+            Console.WriteLine("d =division");
+            Console.WriteLine();
+
+           
+
+            string operateur = Console.ReadLine();
+
+            Console.WriteLine("enter number");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+                switch (operateur)
+                {
+                    case "a":
+                        Console.WriteLine(num1 + num2);
+                        break;
+                    case "s":
+                        Console.WriteLine(num1 - num2);
+                        break;
+                    case "m":
+                        Console.WriteLine(num1 * num2);
+                        break;
+                    case "d":
+                        Console.WriteLine(num1 / num2);
+                        break;
+                    defaut: Console.WriteLine("veuillez ressayer");
+                        break;
+                } while ();
+            }
+
+            
+
+            Console.ReadLine();
+
+
+
+
+
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# calculatrice
+ Stage1 papp
